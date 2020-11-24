a7a96a9a610a2386cf7678296c6fc3d66da4267a
Paul <putincevpavel777@gmail.com>
2020-11-24 20:44:23 +0700

    gistogram plot

---
+++ die_visual.py
@@ -0,0 +1,17 @@
+from die import Die
+
+
+die = Die()
+
+results = []
+for roll_num in  range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Анализ результатов
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
