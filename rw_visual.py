a6ffa442db240ab635a22f17d2bb1adc3efe8bb2
Paul <twinencrypt@gmail.com>
2020-11-25 10:02:14 +0700

    Test module MatPlotLib

--- rw_visual.py
+++ rw_visual.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
+
 from random_walk import RandomWalk
 
 # Новые блуждания строятся до тех пор, пока программа остается активной.
 while True:
     # Построение случайного блуждания и нанесение точек на диаграмму.
-    rw = RandomWalk()
+    rw = RandomWalk(500)
     rw.fill_walk()
 
     # Размер графика
-    plt.figure(figsize=(10, 6))
+    fig = plt.figure(figsize=(10, 6))
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, s=1, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
+    plt.scatter(rw.x_values, rw.y_values, s=10, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none')
 
     # Рисуем начальную и конечную точки
     plt.scatter(rw.x_values[0], rw.x_values[0], s=20, c='green')
@@ -19,6 +20,7 @@ while True:
     # Убираем видимость осей
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
